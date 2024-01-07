c61fa4530274e9515a673865dbe72aed93311f27
riyaz <riyaz3582@gmail.com>
2024-01-07 14:55:04 +0600

    installed react router, firebase, tailwind, daisyUI

---
+++ src/Authentication/firebase.config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAFyJZo2lBhKysYIm6RSXCqy1jjQUtX4ak",
+  authDomain: "time-tracker-2e0c0.firebaseapp.com",
+  projectId: "time-tracker-2e0c0",
+  storageBucket: "time-tracker-2e0c0.appspot.com",
+  messagingSenderId: "456150918163",
+  appId: "1:456150918163:web:022b8f7edd330f36430be6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export default auth
